71314905a432ac0c2a50dfc381dd20da5cb78355
RmiliYahya1 <rmiliyahya2@gmail.com>
2025-12-29 15:41:34 +0100

    first commit

---
+++ microservices-messaging-consumer/src/main/java/com/example/microservicesmessagingconsumer/MicroservicesMessagingConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.example.microservicesmessagingconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMessagingConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesMessagingConsumerApplication.class, args);
+    }
+}
